c66806ec6fd5e8e89edec4aac8a714e82915339e
haykodarb <work@hayk.ar>
2023-06-03 14:32:09 -0300

    Commit before mergin page and controller files

--- lib/presentation/community/community_specific/community_members/community_members_page.dart
+++ lib/presentation/community/community_specific/community_members/community_members_page.dart
@@ -1,8 +1,58 @@
 import 'package:communal/models/profile.dart';
 import 'package:communal/presentation/common/common_loading_body.dart';
-import 'package:communal/presentation/community/community_specific/community_members/community_members_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:communal/backend/users_backend.dart';
+import 'package:communal/models/backend_response.dart';
+import 'package:communal/models/community.dart';
+
+class CommunityMembersController extends GetxController {
+  final Community community = Get.arguments['community'];
+
+  final RxList<Profile> listOfMembers = <Profile>[].obs;
+
+  final RxBool loading = true.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    loadUsers();
+  }
+
+  Future<void> removeUser(Profile user) async {
+    user.loading.value = true;
+
+    final BackendResponse response = await UsersBackend.removeUserFromCommunity(community, user);
+
+    if (response.success) {
+      listOfMembers.remove(user);
+      user.loading.value = false;
+    }
+  }
+
+  Future<void> changeUserAdmin(Profile user, bool shouldBeAdmin) async {
+    user.loading.value = true;
+
+    final BackendResponse response = await UsersBackend.makeUserAdminOfCommunity(community, user, shouldBeAdmin);
+
+    if (response.success) {
+      user.is_admin = shouldBeAdmin;
+      user.loading.value = false;
+    }
+  }
+
+  Future<void> loadUsers() async {
+    loading.value = true;
+
+    final BackendResponse<List<Profile>> response = await UsersBackend.getUsersInCommunity(community);
+
+    if (response.success) {
+      listOfMembers.value = response.payload;
+    }
+
+    loading.value = false;
+  }
+}
 
 class CommunityMembersPage extends StatelessWidget {
   const CommunityMembersPage({super.key});
